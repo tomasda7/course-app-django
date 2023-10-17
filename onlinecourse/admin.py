83f67773a626b550cb12d42b1bfa32fee631adcd
tomasda7 <tomasda7@outlook.com>
2023-10-17 16:12:36 -0400

    registered models: Question, Choice and Submission

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -23,12 +23,13 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
-
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ['content']
+    
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
 
 
 # <HINT> Register Question and Choice models here

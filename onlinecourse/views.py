1b2d3e885f503ba675cedd2d2a8224497bcf671e
tomasda7 <tomasda7@outlook.com>
2023-10-17 19:58:23 -0400

    Updated: exam result template

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -104,6 +104,17 @@ def enroll(request, course_id):
 
 # <HINT> Create a submit view to create an exam submission record for a course enrollment.
 
+
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+    choices = extract_answers(request)
+    submission.choices.set(choices)
+    submission_id = submission.id
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id)))
+
 # An example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
    submitted_anwsers = []
@@ -113,16 +124,6 @@ def extract_answers(request):
            choice_id = int(value)
            submitted_anwsers.append(choice_id)
    return submitted_anwsers
-
-def submit(request, course_id):
-    user = request.user
-    course = get_object_or_404(Course, pk=course_id)
-    enrollment = Enrollment.objects.get(user=user, course=course)
-    submission = Submission.object.create(enrollment=enrollment)
-    choices = extract_answers(request)
-    submission.choices.set(choices)
-    submission_id = submission_id
-    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id)))
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question.
 def show_exam_result(request, course_id, submission_id):
@@ -135,7 +136,7 @@ def show_exam_result(request, course_id, submission_id):
         if choice.is_correct:
             total_score += choice.question.grade
     context['course'] = course
-    context['score'] = total_score
+    context['grade'] = total_score
     context['choices'] = choices
 
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
